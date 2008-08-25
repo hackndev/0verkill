6487dfde4c4bc8955889d401d2f10691ebb129cc
Michal Schmidt <mschmidt@redhat.com>
2008-08-25 02:57:39 +0200

    get rid of acconfig.h
    Usage of acconfig.h is obsolete. Use AH_TEMPLATE to make configure.in
    self-sufficient.

--- acconfig.h
+++
@@ -1,8 +0,0 @@
-/* Define if you have the X11 library (-lX11).  */
-#undef HAVE_LIBX11
-
-/* Define if you have the Xpm library (-lXpm).  */
-#undef HAVE_LIBXPM
-
-/* Define if you have the typeof function. */
-#undef HAVE_TYPEOF
